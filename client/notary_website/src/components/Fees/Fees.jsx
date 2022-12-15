f9d33ba428949316aca7da4d6b8beb1a0ccfcc72
sydneynguyen03 <sydneynguyen03@gmail.com>
2022-12-14 22:44:56 -0800

    fixed the fees page to render

--- client/notary_website/src/components/Fees/Fees.jsx
+++ client/notary_website/src/components/Fees/Fees.jsx
@@ -76,7 +76,7 @@ async function handlePlaceSelect(setQuery, setZip) {
 
 const Fees = () => {
     // VARIABLES TO DISPLAY RESULTS WITH
-    let url = "https://snnotary-website.herokuapp.com/fees"
+    let url = "https://snnotarywebsite.onrender.com/fees"
     // let url = "https://localhost:5000/fees"
 
     const mortgageOptions = ["Misc.", "Single Page", "Refinance", "Purchase", "Seller's Package", "Home Equity Loan", "Reverse Mortgage"]
